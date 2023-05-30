6f5520518607fef85c56b33aa847c8c91ea5b4c9
Omkar Agarkar <omkaragarkar005@gmail.com>
2023-05-30 21:29:22 +0530

    bfs

--- bfs.cpp
+++ bfs.cpp
@@ -1,71 +1,145 @@
-#include <iostream>
+#include<iostream>
 #include <array>
 #include <chrono>
-#include <cstdlib>
-
-void sequentialBubbleSort(int *arr, int size)
+#include <functional>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+string timeCalulator(function<void()> traversal_fn)
 {
-    for (int i = 0; i < size - 1; ++i)
-    {
-        for (int j = 0; j < size - i - 1; ++j)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                std::swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
+auto start = chrono::high_resolution_clock::now();
+traversal_fn();
+auto stop = chrono::high_resolution_clock::now();
+auto result = stop - start;
+auto duration = chrono::duration_cast<chrono::microseconds>(result);
+return to_string(duration.count());
 }
-
-void parallelBubbleSort(int *arr, int size)
+// Direction vectors
+int dRow[] = { -1, 0, 1, 0 };
+int dCol[] = { 0, 1, 0, -1 };
+// Function to check if a cell
+// is be visited or not
+bool isValid(bool vis[][2],int row, int col)
 {
-#pragma omp parallel for
-    for (int i = 0; i < size - 1; ++i)
-    {
-#pragma omp parallel for
-        for (int j = 0; j < size - i - 1; ++j)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                std::swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
+ // If cell lies out of bounds
+ if (row < 0 || col < 0 || col >= 2)
+ return false;
+ // If cell is already visited
+ if (vis[row][col])
+ return false;
+ // Otherwise
+ return true;
 }
-
-std::string calculateExecutionTime(void (*sortFn)(int *, int), int *arr, int size)
+// Function to perform sequential BFS traversal
+void s_BFS(int grid[][2], bool vis[][2],int row, int col)
 {
-    auto start = std::chrono::high_resolution_clock::now();
-    sortFn(arr, size);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    return std::to_string(duration.count());
+ // Stores indices of the matrix cells
+ queue<pair<int, int> > q;
+ // Mark the starting cell as visited
+ // and push it into the queue
+ q.push({ row, col });
+ vis[row][col] = true;
+ // Iterate while the queue
+ // is not empty
+ while (!q.empty()) {
+ pair<int, int> cell = q.front();
+ int x = cell.first;
+ int y = cell.second;
+ q.pop();
+ // Go to the adjacent cells
+ for (int i = 0; i < 4; i++) {
+ int adjx = x + dRow[i];
+ int adjy = y + dCol[i];
+ if (isValid(vis, adjx, adjy)) {
+ q.push({ adjx, adjy });
+ vis[adjx][adjy] = true;
+ }
+ }
+ }
+}
+// Function to perform parallel BFS traversal
+void p_BFS(int grid[][2], bool vis[][2],int row, int col)
+{
+ // Stores indices of the matrix cells
+ queue<pair<int, int> > q;
+ // Mark the starting cell as visited
+ // and push it into the queue
+ q.push({ row, col });
+ vis[row][col] = true;
+ // Iterate while the queue
+ // is not empty
+ #pragma omp parallel num_threads(2)
+ while (!q.empty()) {
+ pair<int, int> cell = q.front();
+ int x = cell.first;
+ int y = cell.second;
+ //cout << grid[x][y] << " ";
+ q.pop();
+ // Go to the adjacent cells
+ #pragma omp parallel for
+ for (int i = 0; i < 4; i++) {
+ int adjx = x + dRow[i];
+ int adjy = y + dCol[i];
+ if (isValid(vis, adjx, adjy)) {
+ q.push({ adjx, adjy });
+ vis[adjx][adjy] = true;
+ }
+ }
+ }
+}
+//Function for generating a graph and calling the traversal function
+void Graphs(int NOEdge, int NOVertex)
+{
+int i, j, edge[NOEdge][2], count;
+i = 0;
+
+ // Assign random values to the number of vertex and edges of the graph,Using rand().
+while (i < NOEdge) {
+edge[i][0] = rand() % NOVertex + 1;
+edge[i][1] = rand() % NOVertex + 1;
+
+ // Print the connections of each vertex, irrespective of the direction.
+if (edge[i][0] == edge[i][1])
+continue;
+else {
+for (j = 0; j < i; j++) {
+if ((edge[i][0] == edge[j][0] && edge[i][1] == edge[j][1]) ||
+(edge[i][0] == edge[j][1]
+&& edge[i][1] == edge[j][0]))
+ i--;
+ }
+ }
+ i++;
+ }
+
+ // Declare the visited array
+ bool vis[NOEdge][2];
+ memset(vis, false, sizeof vis);
+
+ //calling the function for sequential traversal
+ string time1 = timeCalulator([&]{ s_BFS(edge, vis, NOEdge, 2); });
+ cout << "\nSequential BFS Traveral " << time1 << " microseconds\n";
+
+ //calling the function for parallel traversal
+ string time2 = timeCalulator([&]{ p_BFS(edge, vis, NOEdge, 2); });
+ cout << "\nParallel BFS Traversal " << time2 << " microseconds\n";
+
 }
 
 int main()
 {
-    int size;
-    std::cout << "Specify array length: ";
-    std::cin >> size;
+ int i, e, n;
 
-    int *arr = new int[size];
+ cout << "Enter the no of vertices" << endl;
+ cin >> n;
 
-    // Creating an array of random numbers for testing purpose
-    srand(time(0));
-    for (int i = 0; i < size; ++i)
-    {
-        arr[i] = rand() % size;
-    }
+ cout << "The graph has " << n << " vertices" << endl;
 
-    std::cout << "Generated random array of length " << size << "\n\n";
+ e = rand() % ((n * (n - 1)) / 4);
+ cout << "and has " << e << " edges." << endl;
 
-    // Calling the function and calculating the time for sequential bubble sort
-    std::cout << "Sequential Bubble Sort: " << calculateExecutionTime(sequentialBubbleSort, arr, size) << "ms\n";
+ // Function call
+ Graphs(e, n);
 
-    // Calling the function and calculating the time for parallel bubble sort
-    std::cout << "Parallel Bubble Sort: " << calculateExecutionTime(parallelBubbleSort, arr, size) << "ms\n";
-
-    delete[] arr;
-
-    return 0;
+ return 0;
 }
